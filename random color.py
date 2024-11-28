23bf5ee818b469bec88a184ba262e9eb5c1e486d
GevoStudios <164837931+GevoStudios@users.noreply.github.com>
2024-11-28 12:07:53 +0700

    Add files via upload

---
+++ random color.py
@@ -0,0 +1,12 @@
+import random
+color = random.randint(1, 5)
+if color == 1:
+    print("Your color is Red!")
+if color == 2:
+    print("Your color is Yellow!")
+if color == 3:
+    print("Your color is Blue!")
+if color == 4:
+    print("Your color is Green!")
+if color == 5:
+  print("Your color is Pink!")
